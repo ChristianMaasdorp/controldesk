bd879bf8c52d49fe5ffb973f80dbf2b8b9cdb48d
Jacquestrdx123 <jacques.trdx@gmail.com>
2025-04-15 11:11:33 +0200

    Adding

--- app/Services/GithubService.php
+++ app/Services/GithubService.php
@@ -9,10 +9,15 @@ class GithubService{
         $client = new Client();
         $client->authenticate(config('github.token'), null, Client::AUTH_ACCESS_TOKEN);
 // Get list of branches
-        $branches = $client->repo()->branches('owner', 'repo');
+        $branches = $client->repo()->branches('jacquestrdx123', 'CibaRebuildSystem');
 
+        $commits=[];
 // Get commits for a branch
-        $commits = $client->repo()->commits()->all('owner', 'repo', ['sha' => 'branch-name']);
+        foreach($branches as $branch){
+            dd($branch);
+            $commits[] = $client->repo()->commits()->all('owner', 'repo', ['sha' => 'branch-name']);
+
+        }
 
 // Get a single commit
         dd($branches,$commits);
