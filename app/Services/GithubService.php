622fd23e98840e8453f13d2490b80f121f37df03
Jacquestrdx123 <jacques.trdx@gmail.com>
2025-04-15 10:49:28 +0200

    Adding

---
+++ app/Services/GithubService.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Services;
+use Github\Client;
+
+class GithubService{
+    public function syncGithub($user){
+
+        $client = new Client();
+        $client->authenticate(config('github.token'), null, Client::AUTH_ACCESS_TOKEN);
+// Get list of branches
+        $branches = $client->repo()->branches('owner', 'repo');
+
+// Get commits for a branch
+        $commits = $client->repo()->commits()->all('owner', 'repo', ['sha' => 'branch-name']);
+
+// Get a single commit
+        dd($branches,$commits);
+    }
+}
